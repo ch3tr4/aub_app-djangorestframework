2655da7b51a45f2f564045af4c9dda09c83b522b
ch3tr4 <ch3tr4@gmail.com>
2026-01-06 02:29:09 +0700

    Update Settings

--- aub_backend/settings.py
+++ aub_backend/settings.py
@@ -139,6 +139,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 # ======================
